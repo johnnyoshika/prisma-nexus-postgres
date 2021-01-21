c1d87571d216a9b82d624fb4040911acb364e790
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-20 23:01:22 -0800

    Use prisma

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
+import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import express from 'express';
 const app = express();
 
+const prisma = new PrismaClient();
+
+const getRandomInt = (max: number) =>
+  Math.floor(Math.random() * Math.floor(max));
+
 app.get('/', (req, res) => {
+  (async function main() {
+    await prisma.user.create({
+      data: {
+        name: 'John',
+        email: `john+${getRandomInt(100000)}@prisma.io`,
+        posts: {
+          create: { title: 'Hello World' },
+        },
+        profile: {
+          create: { bio: 'I like turtles' },
+        },
+      },
+    });
+
+    const users = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    });
+    console.log(users);
+  })();
+
   res.send(
     `Hi, process.env.SOME_ENV_VARIABLE is ${process.env.SOME_ENV_VARIABLE}`,
   );
