188acf3782f6fbe979b63b075ab659bb4245bc82
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-28 10:37:49 -0800

    Auto-generated typegen and GraphQL schema

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
+import { server } from './server';
 
 const app = express();
 routes(app);
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 const PORT = process.env.PORT || 8000;
 
