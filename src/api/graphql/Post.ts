3e741321bb087400323b73ec82cbf9de8606174e
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-28 11:19:24 -0800

    Add drafts query field

--- src/api/graphql/Post.ts
+++ src/api/graphql/Post.ts
@@ -1,4 +1,4 @@
-import { objectType } from 'nexus';
+import { extendType, objectType } from 'nexus';
 
 export const Post = objectType({
   name: 'Post',
@@ -7,5 +7,17 @@ export const Post = objectType({
     t.string('title');
     t.string('body');
     t.boolean('published');
+  },
+});
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('drafts', {
+      type: 'Post',
+      resolve: () => [
+        { id: 1, title: 'Nexus', body: '...', published: false },
+      ],
+    });
   },
 });
