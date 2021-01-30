935b60507a3773511bf69a90b45ffd94bb482c7b
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-29 23:30:44 -0800

    Simplify build process for deployment

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -2,14 +2,19 @@ import { makeSchema } from 'nexus';
 import { join } from 'path';
 import * as types from './graphql';
 
+// Need .ts TypeScript extensions in dev and .js JavaScript extensions
+// once TypeScript is compiled in production
+
+const ext = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
+
 export const schema = makeSchema({
   types,
   outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'), // This needs to be .js extension once TypeScript is compiled to JS
+    typegen: join(__dirname, '..', `nexus-typegen.${ext}`),
     schema: join(__dirname, '..', 'schema.graphql'),
   },
   contextType: {
-    module: join(__dirname, './context.ts'), // This needs to be .js extension once TypeScript is compiled to JS
+    module: join(__dirname, `./context.${ext}`),
     export: 'Context',
   },
 });
